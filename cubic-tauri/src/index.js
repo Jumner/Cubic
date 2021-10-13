29b133f9be82423b6111a9af4773c26b2b5534f9
Jumner <Jumner@debian>
2021-10-13 18:59:50 -0400

    dark mode lol

--- cubic-tauri/src/index.js
+++ cubic-tauri/src/index.js
@@ -7,7 +7,6 @@ import {
 	TerminalIcon,
 	ChipIcon,
 	CheckCircleIcon,
-	ExclamationCircleIcon,
 	XCircleIcon,
 } from '@heroicons/react/solid';
 import './index.css';
@@ -15,22 +14,17 @@ import './index.css';
 function Main() {
 	const [SideBar, setSideBar] = useState(0);
 	function selected(i) {
-		return i === SideBar
-			? 'bg-gray-100 dark:bg-gray-600 text-gray-800 dark:text-gray-100 '
-			: 'text-gray-600 dark:text-gray-400 ';
+		return i === SideBar ? 'bg-gray-600 text-gray-100 ' : 'text-gray-400 ';
 	}
 	const pages = [<Home />, <>a</>, <>b</>, <>c</>];
 	const [Connected, setConnected] = useState(false);
 	return (
-		<div className="relative bg-white dark:bg-gray-800">
+		<div className="relative bg-gray-800">
 			<div className="container">
-				{/* <div className="flex flex-col sm:flex-row sm:justify-around"> */}
-				<div className="w-72 h-screen float-left bg-white dark:bg-gray-800">
+				<div className="w-72 h-screen float-left bg-gray-800">
 					<div className="flex items-center justify-start mx-6 mt-10">
-						<CubeIcon width="30" className="ml-1" />
-						<span className="text-gray-600 dark:text-gray-300 ml-4 text-2xl font-bold">
-							Cubic
-						</span>
+						<CubeIcon width="30" className="ml-1 text-gray-300" />
+						<span className="text-gray-300 ml-4 text-2xl font-bold">Cubic</span>
 					</div>
 					<nav className="mt-10 px-6 ">
 						<button
@@ -39,7 +33,7 @@ function Main() {
 							}}
 							className={
 								selected(0) +
-								'hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200 rounded-lg'
+								'flex items-center p-2 my-6 transition-colors hover:text-white hover:bg-gray-600 duration-200 rounded-lg'
 							}
 						>
 							<ChipIcon
@@ -56,7 +50,7 @@ function Main() {
 							}}
 							className={
 								selected(1) +
-								'hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200 rounded-lg'
+								'flex items-center p-2 my-6 transition-colors hover:text-white hover:bg-gray-600 duration-200 rounded-lg'
 							}
 						>
 							<TerminalIcon
@@ -73,7 +67,7 @@ function Main() {
 							}}
 							className={
 								selected(2) +
-								'hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200 rounded-lg'
+								'flex items-center p-2 my-6 transition-colors hover:text-white hover:bg-gray-600 duration-200 rounded-lg'
 							}
 						>
 							<ChartBarIcon
@@ -87,10 +81,11 @@ function Main() {
 						<button
 							onClick={() => {
 								setSideBar(3);
+								setConnected(!Connected);
 							}}
 							className={
 								selected(3) +
-								'hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200 rounded-lg'
+								'flex items-center p-2 my-6 transition-colors hover:text-white hover:bg-gray-600 duration-200 rounded-lg'
 							}
 						>
 							{Connected ? (
