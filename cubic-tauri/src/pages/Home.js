a52ab08fc202cab79d386c7fcbd4c593e2221e8e
Jumner <Jumner@debian>
2021-10-13 23:10:20 -0400

    added stuff for graphs

--- cubic-tauri/src/pages/Home.js
+++ cubic-tauri/src/pages/Home.js
@@ -8,32 +8,32 @@ function Box(props) {
 	useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.004));
 	return (
 		<mesh {...props} ref={mesh}>
-			{' '}
-			<boxGeometry args={[3, 3, 3]} /> <meshStandardMaterial color={0xff6000} />{' '}
+			<boxGeometry args={[3, 3, 3]} />
+			<meshStandardMaterial color={0xff6000} />
 		</mesh>
 	);
 }
 
 function Home() {
 	return (
-		<div className="flex min-h-screen justify-center items-center">
-			<div className="absolute top-0 bottom-0 right-0 left-64">
+		<>
+			<div className="absolute top-0 bottom-52 right-0 left-64">
 				<Canvas>
 					<ambientLight intensity={0.3} />
-					<spotLight position={[0, 10, 0]} intensity={1.5} angle={Math.PI} />
-					<spotLight
-						position={[0, -10, 0]}
-						angle={Math.PI}
-						lookAt={[0, 0, 0]}
-					/>
+					<spotLight position={[0, 10, 0]} />
+					<spotLight position={[0, -10, 0]} />
 					<Box position={[0, 0, 0]} />
 				</Canvas>
 			</div>
-
-			<div className="grid grid-rows-2 gap-10 min-w-full min-h-screen">
-				<p onClick={() => {}}>Overview</p>
+			<div className="left-64 right-0 absolute bottom-0 h-52">
+				<div className="flex items-center justify-around h-full">
+					<p onClick={() => {}}>Overview</p>
+					<p onClick={() => {}}>Overview</p>
+					<p onClick={() => {}}>Overview</p>
+				</div>
 			</div>
-		</div>
+			{/* </div> */}
+		</>
 	);
 }
 
