e0186a4d706105f5475179237a6c87b217ba5790
Jumner <Jumner@debian>
2021-10-23 01:19:08 -0400

    weird rerender bug

--- cubic-tauri/src/pages/Status.js
+++ cubic-tauri/src/pages/Status.js
@@ -1,8 +1,5 @@
 import { BanIcon, ChipIcon } from '@heroicons/react/solid';
-import React, { useEffect, useState } from 'react';
-// Graphs
-import { Legend, Line, LineChart, Tooltip, YAxis } from 'recharts';
-import { setTelemCallback } from '../index';
+import { Graph } from '../graph';
 //https://recharts.org/en-US/guide/getting-started
 
 function Status(props) {
@@ -60,44 +57,6 @@ function Status(props) {
 			<div className="container bg-green-500 flex items-center text-white text-lg font-bold px-4 py-3 self-start absolute w-full">
 				<ChipIcon fill="currentColor" className="w-6 h-6 mr-3" />
 				Awesome! Connected to Cubic successfully
-			</div>
-		);
-	}
-	const [data, setData] = useState([]);
-	function addData(data) {
-		setData(oldData => {
-			return [...oldData, data].slice(oldData.length >= 5 ? 1 : 0);
-		});
-	}
-	function Graph() {
-		function telemCallback(val) {
-			if (data.length === 0 || val !== data[data.length - 1].a) {
-				console.log('Added data', val);
-				addData({ name: 't', a: val });
-			}
-		}
-		useEffect(() => {
-			console.log('mount graph');
-			setTelemCallback('statusGraph', telemCallback);
-		}, []);
-		return (
-			<div className="bg-gray-800 shadow-lg rounded-xl p-4 flex m-auto">
-				<LineChart
-					width={200}
-					height={200}
-					data={data}
-					margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-				>
-					<Line
-						type="monotone"
-						dataKey="a"
-						dot={false}
-						isAnimationActive={true}
-					/>
-					<YAxis />
-					<Tooltip />
-					<Legend />
-				</LineChart>
 			</div>
 		);
 	}
