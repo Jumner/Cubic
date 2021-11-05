7fa63c81c0026ae8b5f1bacc0ad07407e2f4dcf5
Jumner Chromebook <1frankjus@hdsb.ca>
2021-11-05 16:43:01 -0400

    starting on bluetooth

--- cubic/src/main.rs
+++ cubic/src/main.rs
@@ -1,11 +1,23 @@
+use blurz::{
+	BluetoothAdapter, BluetoothDevice, BluetoothDiscoverySession, BluetoothEvent,
+	BluetoothGATTCharacteristic, BluetoothSession,
+};
 fn main() {
-	println!("Hello, world!");
+	let bt_session = &BluetoothSession::create_session(None).unwrap();
+	let adapter: BluetoothAdapter = BluetoothAdapter::init(bt_session).unwrap();
+	let adapter_id = adapter.get_id();
+	let discover_session =
+		BluetoothDiscoverySession::create_session(&bt_session, adapter_id).unwrap();
+	discover_session.start_discovery().unwrap();
+	let device_list = adapter.get_device_list().unwrap();
+	discover_session.stop_discovery().unwrap();
+	println!("Hello, world!\n{:?}", device_list);
 }
 
 #[cfg(test)]
 mod tests {
 	// Tests module
-	use super::*;
+	// use super::*;
 
 	#[test]
 	fn it_works() {
