0d2bcedcd6f0f4c5f06480683eaf2a4c0b3fb30e
JumnerArch <1frankjus@hdsb.ca>
2021-11-06 14:35:09 -0400

    why is ble so weird

--- cubic/src/main.rs
+++ cubic/src/main.rs
@@ -1,17 +1,52 @@
-use blurz::{
-	BluetoothAdapter, BluetoothDevice, BluetoothDiscoverySession, BluetoothEvent,
-	BluetoothGATTCharacteristic, BluetoothSession,
+use btleplug::api::{
+	bleuuid::uuid_from_u16, Central, Manager as _, Peripheral as _, ScanFilter, WriteType,
 };
-fn main() {
-	let bt_session = &BluetoothSession::create_session(None).unwrap();
-	let adapter: BluetoothAdapter = BluetoothAdapter::init(bt_session).unwrap();
-	let adapter_id = adapter.get_id();
-	let discover_session =
-		BluetoothDiscoverySession::create_session(&bt_session, adapter_id).unwrap();
-	discover_session.start_discovery().unwrap();
-	let device_list = adapter.get_device_list().unwrap();
-	discover_session.stop_discovery().unwrap();
-	println!("Hello, world!\n{:?}", device_list);
+use btleplug::platform::{Adapter, Manager, Peripheral};
+use rand::{thread_rng, Rng};
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+use tokio::process::Command;
+use tokio::time;
+use uuid::Uuid;
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+	let manager = Manager::new().await.unwrap();
+	let adapters = manager.adapters().await.expect("Unable to get adapters");
+	let central = adapters
+		.into_iter()
+		.nth(0)
+		.expect("Unable to find adapters");
+
+	central
+		.start_scan(ScanFilter::default())
+		.await
+		.expect("Unable to scan");
+	time::delay_for(Duration::from_secs(2)).await;
+
+	let cubic = find_cubic(&central).await.expect("Could not find cubic");
+	cubic.connect().await.expect("Could not connect to cubic");
+	// cubic
+	// 	.properties()
+	// 	.await
+	// 	.unwrap()
+	// 	.unwrap()
+	// 	.services
+	// 	.iter()
+	// 	.for_each(|x| println!("{:?}", x));
+	// cubic.discover_services().await?;
+	// cubic.services().iter().for_each(|x| {
+	// 	println!("{}", x.uuid);
+	// });
+
+	let out = Command::new("date")
+		// .args(&["hello", "world"])
+		.output()
+		.await?;
+	let s = String::from_utf8_lossy(&out.stdout);
+	println!("{}", s);
+	println!("Hello, world!");
+	Ok(())
 }
 
 #[cfg(test)]
@@ -23,4 +58,21 @@ mod tests {
 	fn it_works() {
 		assert_eq!(4, 2 + 2);
 	}
+}
+
+async fn find_cubic(central: &Adapter) -> Option<Peripheral> {
+	for p in central.peripherals().await.unwrap() {
+		if p
+			.properties()
+			.await
+			.unwrap()
+			.unwrap()
+			.local_name
+			.iter()
+			.any(|name| name.contains("hc-05"))
+		{
+			return Some(p);
+		}
+	}
+	None
 }
