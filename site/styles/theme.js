a398a4b521d5440ddf4cca240e3cd9b0bbe2c1c5
JumnerArch <1frankjus@hdsb.ca>
2021-12-16 17:25:27 -0500

    done template

---
+++ site/styles/theme.js
@@ -0,0 +1,9 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const colours = {
+	gray: '#405059',
+	blue: '#0c8f8f',
+	green: '#73b06f',
+};
+
+export const theme = extendTheme({ colors: colours });
