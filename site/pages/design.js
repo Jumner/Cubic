1bbefc5573f9178011a7d9b47f22763e7d9dcfb9
Jumner Chromebook <1frankjus@hdsb.ca>
2021-12-25 12:36:45 -0500

    worked on design

--- site/pages/design.js
+++ site/pages/design.js
@@ -1,15 +1,24 @@
-import { Heading, Image, Text } from '@chakra-ui/react';
-import Page from './components/Page';
-import Render from './../public/img/finalrender.png';
+import { Heading, Image, Text } from "@chakra-ui/react";
+import Page from "./components/Page";
+import Render from "./../public/img/finalrender.png";
 export default function Design() {
-	return (
-		<Page name="Design">
-			<Heading fontSize="6xl">Design</Heading>
-			<Text fontSize="large">
-				Because this of the nature of this project, everything needed to be
-				designed from scratch
-			</Text>
-			<Image src={Render.src} w="lg" alt="Render" />
-		</Page>
-	);
+  return (
+    <Page name="Design">
+      <Heading fontSize="6xl">Design</Heading>
+      <Text fontSize="large">
+        Because this of the nature of this project, everything needed to be
+        designed from scratch
+      </Text>
+      <Image src={Render.src} w="lg" alt="Render" />
+      <Text>
+        Because of the size of the motors, the minimum size of cube is 150mm.
+        This is a problem because the whole thing needs to be 3d printed and my
+        3d printer has a max size of 120mm
+      </Text>
+      <Text>
+        The solution is to build it in multiple peices, 112 peices to be exact.
+      </Text>
+	<Text></Text>
+    </Page>
+  );
 }
